d55639e9bbd2feaf9a4b8dfd9cc7151c694f0b8b
Yutaka HARA <yutaka.hara+github@gmail.com>
2024-02-04 13:47:38 +0900

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+MLIR experiment
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use melior::{
+    Context,
+    dialect::{arith, DialectRegistry, func},
+    ir::{*, attribute::{StringAttribute, TypeAttribute}, r#type::FunctionType},
+    utility::{register_all_dialects, register_all_llvm_translations},
+    pass::{self, PassManager},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let registry = DialectRegistry::new();
+    register_all_dialects(&registry);
+
+    let context = Context::new();
+    context.append_dialect_registry(&registry);
+    context.load_all_available_dialects();
+    register_all_llvm_translations(&context);
+
+    let location = Location::unknown(&context);
+    let mut module = Module::new(location);
+
+    let index_type = Type::index(&context);
+
+    module.body().append_operation(func::func(
+        &context,
+        StringAttribute::new(&context, "add"),
+        TypeAttribute::new(FunctionType::new(&context, &[index_type, index_type], &[index_type]).into()),
+        {
+            let block = Block::new(&[(index_type, location), (index_type, location)]);
+
+            let sum = block.append_operation(arith::addi(
+                block.argument(0).unwrap().into(),
+                block.argument(1).unwrap().into(),
+                location
+            ));
+
+            block.append_operation(func::r#return( &[sum.result(0).unwrap().into()], location));
+
+            let region = Region::new();
+            region.append_block(block);
+            region
+        },
+        &[],
+        location,
+    ));
+
+    assert!(module.as_operation().verify());
+    module.as_operation().dump();
+    println!("--");
+
+    let pass_manager = PassManager::new(&context);
+    pass_manager.add_pass(pass::conversion::create_func_to_llvm());
+    pass_manager
+        .nested_under("func.func")
+        .add_pass(pass::conversion::create_arith_to_llvm());
+    pass_manager
+        .nested_under("func.func")
+        .add_pass(pass::conversion::create_index_to_llvm());
+    pass_manager.add_pass(pass::conversion::create_scf_to_control_flow());
+    pass_manager.add_pass(pass::conversion::create_control_flow_to_llvm());
+    pass_manager.add_pass(pass::conversion::create_finalize_mem_ref_to_llvm());
+    pass_manager.run(&mut module).unwrap();
+
+
+    assert!(module.as_operation().verify());
+    module.as_operation().dump();
+
 }
