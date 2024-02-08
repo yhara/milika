54e78826104a6ed6035e9430264ed523f21e2736
Yutaka Hara <yutaka.hara+github@gmail.com>
2024-02-08 17:42:52 +0900

    .

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,12 +1,12 @@
 use crate::ast;
 use crate::asyncness_check::gather_sigs;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use melior::{
     dialect::{self, DialectRegistry},
     ir::{
+        self,
         attribute::{StringAttribute, TypeAttribute},
         r#type::FunctionType,
-        Block, Identifier, Location, Module, Operation, Region, Type,
     },
     pass::{self, PassManager},
     utility::{register_all_dialects, register_all_llvm_translations},
@@ -38,7 +38,7 @@ pub fn run(ast: ast::Program) -> Result<()> {
 
 impl Compiler {
     fn compile_program(&mut self, ast: ast::Program) -> Result<()> {
-        let mut module = Module::new(self.location());
+        let mut module = ir::Module::new(self.location());
 
         for decl in ast {
             match decl {
@@ -46,7 +46,7 @@ impl Compiler {
                     module.body().append_operation(self.compile_extern(e)?);
                 }
                 ast::Declaration::Function(f) => {
-                    module.body().append_operation(self.compile_func(f)?);
+                    module.body().append_operation(self.compile_func(f, true)?);
                 }
             }
         }
@@ -73,37 +73,68 @@ impl Compiler {
         Ok(())
     }
 
-    fn compile_extern(&self, ext: ast::Extern) -> Result<Operation> {
-        self.compile_func(ext.into_empty_func())
-    }
-
-    fn compile_func(&self, func: ast::Function) -> Result<Operation> {
-        let index_type = Type::index(&self.context);
-        let block = Block::new(&[(index_type, self.location()), (index_type, self.location())]);
-        let sum = block.append_operation(dialect::arith::addi(
-            block.argument(0).unwrap().into(),
-            block.argument(1).unwrap().into(),
-            self.location(),
-        ));
-
-        block.append_operation(dialect::func::r#return(
-            &[sum.result(0).unwrap().into()],
-            self.location(),
-        ));
-
-        let region = Region::new();
-        region.append_block(block);
-
+    fn compile_extern(&self, ext: ast::Extern) -> Result<ir::Operation> {
+        let attrs = vec![(
+                self.identifier("sym_visibility"),
+                self.str_attr("private").into(),
+            )];
         Ok(dialect::func::func(
             &self.context,
-            self.str_attr(&func.name),
-            TypeAttribute::new(
-                FunctionType::new(&self.context, &[index_type, index_type], &[index_type]).into(),
-            ),
-            region,
-            &[],
+            self.str_attr(&ext.name),
+            TypeAttribute::new(self.function_type(&ext.fun_ty())?),
+            Default::default(),
+            &attrs,
             self.location(),
         ))
+    }
+
+    fn compile_func(&self, f: ast::Function, is_extern: bool) -> Result<ir::Operation> {
+        let index_type = ir::Type::index(&self.context);
+        let block = self.create_main_block(&f)?;
+        //ir::Block::new(&[(index_type, self.location()), (index_type, self.location())]);
+        //let sum = block.append_operation(dialect::arith::addi(
+        //    block.argument(0).unwrap().into(),
+        //    block.argument(1).unwrap().into(),
+        //    self.location(),
+        //));
+
+        //block.append_operation(dialect::func::r#return(
+        //    &[sum.result(0).unwrap().into()],
+        //    self.location(),
+        //));
+
+        let region = ir::Region::new();
+        region.append_block(block);
+
+        let attrs = if is_extern {
+            vec![(
+                self.identifier("sym_visibility"),
+                self.str_attr("private").into(),
+            )]
+        } else {
+            vec![]
+        };
+        Ok(dialect::func::func(
+            &self.context,
+            self.str_attr(&f.name),
+            TypeAttribute::new(self.function_type(&f.fun_ty(false))?),
+            region,
+            &attrs,
+            self.location(),
+        ))
+    }
+
+    fn create_main_block(&self, f: &ast::Function) -> Result<ir::Block> {
+        let param_tys = f
+            .params
+            .iter()
+            .map(|x| self.mlir_type(&x.ty))
+            .collect::<Result<Vec<_>>>()?
+            .into_iter()
+            .map(|x| (x, self.location()))
+            .collect::<Vec<_>>();
+        //let ret_ty = (self.mlir_type(&f.ret_ty)?, self.location());
+        Ok(ir::Block::new(&param_tys))
     }
 
     //    fn compile_expr(&self, expr: ast::Expr) -> todo {
@@ -113,15 +144,36 @@ impl Compiler {
     //        }
     //    }
 
-    fn identifier(&self, s: &str) -> Identifier {
-        Identifier::new(&self.context, s)
+    fn function_type(&self, fun_ty: &ast::FunTy) -> Result<ir::Type> {
+        let param_tys = fun_ty
+            .param_tys
+            .iter()
+            .map(|x| self.mlir_type(x))
+            .collect::<Result<Vec<_>>>()?;
+        let ret_ty = self.mlir_type(&fun_ty.ret_ty)?;
+        Ok(FunctionType::new(&self.context, &param_tys, &[ret_ty]).into())
+    }
+
+    fn mlir_type(&self, ty: &ast::Ty) -> Result<ir::Type> {
+        let t = match ty {
+            ast::Ty::Raw(s) => match &s[..] {
+                "int" => ir::r#type::IntegerType::signed(&self.context, 64).into(),
+                _ => return Err(anyhow!("unknown type `{}'", s)),
+            },
+            ast::Ty::Fun(fun_ty) => return self.function_type(fun_ty),
+        };
+        Ok(t)
+    }
+
+    fn identifier(&self, s: &str) -> ir::Identifier {
+        ir::Identifier::new(&self.context, s)
     }
 
     fn str_attr(&self, s: &str) -> StringAttribute {
         StringAttribute::new(&self.context, s)
     }
 
-    fn location(&self) -> Location {
-        Location::unknown(&self.context)
+    fn location(&self) -> ir::Location {
+        ir::Location::unknown(&self.context)
     }
 }
