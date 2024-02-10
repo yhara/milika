cecb1a4423430bfc0fd0e74a757da463aafea0ae
Yutaka HARA <yutaka.hara+github@gmail.com>
2024-02-10 22:17:27 +0900

    .

--- src/compiler.rs
+++ src/compiler.rs
@@ -15,11 +15,19 @@ use std::collections::HashMap;
 
 /// Get the first result value of an operation.
 /// Panics if the operation yields no value
-fn val<'c, 'a>(x: &'a ir::Operation<'c>) -> ir::Value<'c, 'a> {
+fn val<'c, 'a>(x: &'a ir::OperationRef<'c, 'a>) -> ir::Value<'c, 'a> {
     x.result(0)
         .unwrap_or_else(|_| panic!("this operation has no value"))
         .into()
 }
+
+//fn vals<'c>(xs: &'c [ir::OperationRef<'c, 'c>]) -> Vec<ir::Value<'c, 'c>> {
+//    let mut v = vec![];
+//    for x in xs {
+//        v.push(val(x));
+//    }
+//    v
+//}
 
 struct Compiler<'run: 'c, 'c> {
     filename: &'run str,
@@ -47,7 +55,7 @@ pub fn run(filename: &str, src: &str, ast: ast::Program) -> Result<()> {
     Ok(())
 }
 
-impl<'run, 'c> Compiler<'run, 'c> {
+impl<'run: 'c, 'c> Compiler<'run, 'c> {
     fn compile_program(&mut self, ast: ast::Program) -> Result<()> {
         let mut module = ir::Module::new(self.unknown_location());
         let block = module.body();
@@ -108,7 +116,8 @@ impl<'run, 'c> Compiler<'run, 'c> {
     ) -> Result<ir::Operation> {
         let block = self.create_main_block(&f)?;
         for stmt in &f.body_stmts {
-            block.append_operation(self.compile_expr(&block, stmt)?);
+            //block.append_operation(self.compile_expr(&block, stmt)?);
+            self.compile_expr(&block, stmt)?;
         }
 
         let region = ir::Region::new();
@@ -148,34 +157,72 @@ impl<'run, 'c> Compiler<'run, 'c> {
     //        self.compile_expr(block, expr)
     //    }
 
-    fn compile_expr<'a: 'run>(
+    fn compile_expr(
         &'run self,
-        block: &'a ir::Block,
+        block: &'c ir::Block,
         expr: &ast::Expr,
-    ) -> Result<ir::Operation<'run>> {
+    ) -> Result<ir::OperationRef<'c, 'c>> {
         let op = match expr {
             ast::Expr::Number(n) => dialect::arith::constant(
                 &self.context,
                 IntegerAttribute::new(*n, IntegerType::signed(&self.context, 64).into()).into(),
                 self.unknown_location(),
             ),
+            ast::Expr::FunCall(fexpr, arg_exprs) => {
+                return self.compile_funcall(block, fexpr, arg_exprs)
+            }
             ast::Expr::Return(val_expr) => {
-                let v = block.append_operation(self.compile_expr(block, val_expr)?);
+                //let v = block.append_operation(self.compile_expr(block, val_expr)?);
+                let v = self.compile_expr(block, val_expr)?;
                 dialect::func::r#return(&[val(&v)], self.unknown_location())
             }
             _ => todo!("{:?}", expr),
         };
-        Ok(op)
+        Ok(block.append_operation(op))
+    }
+
+    fn compile_funcall(
+        &'run self,
+        block: &'c ir::Block,
+        fexpr: &ast::Expr,
+        arg_exprs: &[ast::Expr],
+    ) -> Result<ir::OperationRef<'c, 'c>> {
+        let fun_ty = match fexpr {
+            ast::Expr::VarRef(s) => {
+                if let Some(t) = self.sigs.get(s) {
+                    t
+                } else {
+                    todo!()
+                }
+            }
+            _ => return Err(anyhow!("not a function?")),
+        };
+
+        let f = self.compile_expr(block, fexpr)?;
+
+        let mut args = vec![];
+        for e in arg_exprs {
+            args.push(self.compile_expr(block, e)?.result(0)?.into());
+        }
+
+        let result_types = fun_ty
+            .param_tys
+            .iter()
+            .map(|t| self.mlir_type(t))
+            .collect::<Result<Vec<_>>>()?;
+        let op =
+            dialect::func::call_indirect(val(&f), &args, &result_types, self.unknown_location());
+        Ok(block.append_operation(op))
     }
 
     fn function_type(&self, fun_ty: &ast::FunTy) -> Result<ir::Type> {
-        let param_tys = fun_ty
-            .param_tys
-            .iter()
-            .map(|x| self.mlir_type(x))
-            .collect::<Result<Vec<_>>>()?;
+        let param_tys = self.mlir_types(&fun_ty.param_tys)?;
         let ret_ty = self.mlir_type(&fun_ty.ret_ty)?;
         Ok(FunctionType::new(&self.context, &param_tys, &[ret_ty]).into())
+    }
+
+    fn mlir_types(&self, tys: &[ast::Ty]) -> Result<Vec<ir::Type>> {
+        tys.iter().map(|x| self.mlir_type(x)).collect()
     }
 
     fn mlir_type(&self, ty: &ast::Ty) -> Result<ir::Type> {
