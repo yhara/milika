be233739ce23637d7b1ec7e417f6134fdb0338aa
Yutaka HARA <yutaka.hara+github@gmail.com>
2024-02-12 21:37:16 +0900

    return

--- src/parser.rs
+++ src/parser.rs
@@ -130,7 +130,17 @@ fn parse_block<'a>(s: Span<'a>) -> IResult<Span<'a>, Vec<ast::Expr>, E> {
 
 /// An expr terminated with ';'
 fn parse_stmt<'a>(s: Span<'a>) -> IResult<Span<'a>, ast::Expr, E> {
-    terminated(parse_expr, terminated(multispace0, tag(";")))(s)
+    terminated(
+        alt((parse_return, parse_expr)),
+        terminated(multispace0, tag(";")),
+    )(s)
+}
+
+fn parse_return<'a>(s: Span<'a>) -> IResult<Span<'a>, ast::Expr, E> {
+    let (s, pos) = tag("return")(s)?;
+    let (s, name) = multispace1(s)?;
+    let (s, expr) = parse_expr(s)?;
+    Ok((s, ast::Expr::Return(Box::new(expr))))
 }
 
 fn parse_expr<'a>(s: Span<'a>) -> IResult<Span<'a>, ast::Expr, E> {
@@ -159,136 +169,3 @@ fn parse_ty<'a>(s: Span<'a>) -> IResult<Span<'a>, Spanned<ast::Ty>, E> {
     let (s, name) = alphanumeric1(s)?;
     Ok((s, (ast::Ty::Raw(name.to_string()), pos)))
 }
-
-//fn expr_parser<'a>() -> impl Parser<'a, &'a str, ast::Expr> {
-//    recursive(|expr| {
-//        let atomic = {
-//            let number = just('-')
-//                .or_not()
-//                .chain::<char, _, _>(text::int(10))
-//                .collect::<String>()
-//                .from_str()
-//                .unwrapped()
-//                .map(ast::Expr::Number);
-//
-//            let parenthesized = expr.clone().delimited_by(just('('), just(')'));
-//
-//            let funcall = (varref_parser().or(parenthesized.clone()))
-//                .then_ignore(just('('))
-//                .then(expr.clone().padded().separated_by(just(',')))
-//                .then_ignore(just(')'))
-//                .map(|(func_expr, args)| ast::Expr::FunCall(Box::new(func_expr), args));
-//
-//            funcall.or(parenthesized).or(varref_parser()).or(number)
-//        };
-//        let bin_op = just("==")
-//            .or(just("!="))
-//            .or(just("<="))
-//            .or(just("<"))
-//            .or(just(">="))
-//            .or(just(">"))
-//            .or(just("+"))
-//            .or(just("-"));
-//        let sum = atomic
-//            .clone()
-//            .then(bin_op.padded().then(atomic.clone()).repeated())
-//            .foldl(|lhs, (op, rhs)| {
-//                ast::Expr::OpCall(op.to_string(), Box::new(lhs), Box::new(rhs))
-//            });
-//
-//        let alloc = just("alloc")
-//            .padded()
-//            .ignore_then(ident_parser())
-//            .map(ast::Expr::Alloc);
-//
-//        let retn = just("return")
-//            .padded()
-//            .ignore_then(expr.clone())
-//            .map(|e| ast::Expr::Return(Box::new(e)));
-//
-//        //        let para = just("para")
-//        //            .padded()
-//        //            .ignore_then(
-//
-//        let assign = ident_parser()
-//            .padded()
-//            .then_ignore(just('=').padded())
-//            .then(expr.clone())
-//            .map(|(name, rhs)| ast::Expr::Assign(name, Box::new(rhs)));
-//
-//        alloc.or(retn).or(assign).or(sum).or(atomic)
-//    })
-//}
-//
-//fn stmts_parser<'a>() -> impl Parser<'a, &'a str, Vec<ast::Expr>> {
-//    let comment = just('#').then(take_until(just('\n'))).padded();
-//    expr_parser()
-//        .padded()
-//        .then_ignore(just(';'))
-//        .padded_by(comment.repeated())
-//        .padded()
-//        .repeated()
-//}
-//
-//fn param_parser<'a>() -> impl Parser<'a, &'a str, ast::Param> {
-//    ty_parser()
-//        .padded()
-//        .then(ident_parser())
-//        .map(|(ty, name)| ast::Param { ty, name })
-//}
-//
-//fn params_parser<'a>() -> impl Parser<'a, &'a str, Vec<ast::Param>> {
-//    param_parser().padded().separated_by(just(','))
-//}
-//
-//fn func_parser<'a>() -> impl Parser<'a, &'a str, Spanned<ast::Function>> {
-//    just("fun")
-//        .ignore_then(ident_parser().padded())
-//        .then(params_parser().delimited_by(just('('), just(')')))
-//        .then_ignore(just("->").padded())
-//        .then(ty_parser().padded())
-//        .then(stmts_parser().delimited_by(just('{'), just('}')))
-//        .map_with_span(|(((name, params), ret_ty), body_stmts), span| {
-//            let f = ast::Function {
-//                name,
-//                params,
-//                ret_ty,
-//                body_stmts,
-//            };
-//            (f, span)
-//        })
-//}
-//
-//fn extern_parser<'a>() -> impl Parser<'a, &'a str, Spanned<ast::Extern>> {
-//    just("extern_async")
-//        .or(just("extern"))
-//        .then(ident_parser().padded())
-//        .then(params_parser().delimited_by(just('('), just(')')))
-//        .then_ignore(just("->").padded())
-//        .then(ty_parser().padded())
-//        .then_ignore(just(';').padded())
-//        .map_with_span(|(((is_async, name), params), ret_ty), span| {
-//            let e = ast::Extern {
-//                is_async: is_async == "extern_async",
-//                name,
-//                params,
-//                ret_ty,
-//            };
-//            (e, span)
-//        })
-//}
-//
-//fn decl_parser<'a>() -> impl Parser<'a, &'a str, ast::Declaration> {
-//    func_parser()
-//        .map(ast::Declaration::Function)
-//        .or(extern_parser().map(ast::Declaration::Extern))
-//}
-//
-//pub fn parser<'a>() -> impl Parser<'a, &'a str, ast::Program> {
-//    let comment = just('#').then(take_until(just('\n'))).padded();
-//    decl_parser()
-//        .padded_by(comment.repeated())
-//        .padded()
-//        .repeated()
-//        .then_ignore(end())
-//}
