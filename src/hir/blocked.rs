d09909221a246b0264b42299122f2216095dbe7f
Yutaka HARA <yutaka.hara+github@gmail.com>
2024-05-05 17:57:07 +0900

    impl Display

--- src/hir/blocked.rs
+++ src/hir/blocked.rs
@@ -1,4 +1,5 @@
 use crate::hir;
+use std::fmt;
 
 pub type Block = Vec<hir::Typed<hir::Expr>>;
 
@@ -8,6 +9,18 @@ pub struct Program {
     pub funcs: Vec<Function>,
 }
 
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for e in &self.externs {
+            write!(f, "{}", e)?;
+        }
+        for func in &self.funcs {
+            write!(f, "{}", func)?;
+        }
+        write!(f, "")
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Function {
     pub is_async: Option<bool>, // None means "unknown" or "N/A" depending on the phase
@@ -15,4 +28,23 @@ pub struct Function {
     pub params: Vec<hir::Param>,
     pub ret_ty: hir::Ty,
     pub body_blocks: Vec<Block>,
+}
+
+impl fmt::Display for Function {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let para = self
+            .params
+            .iter()
+            .map(|p| p.to_string())
+            .collect::<Vec<_>>()
+            .join(", ");
+        write!(f, "fun {}({}) -> {} {{\n", self.name, para, self.ret_ty)?;
+        for block in &self.body_blocks {
+            write!(f, "^bb()\n")?;
+            for expr in block {
+                write!(f, "  {};  #-> {}\n", &expr.0, &expr.1)?;
+            }
+        }
+        write!(f, "}}\n")
+    }
 }
