4dba1044ebcf112d18c4edce49d22b7eb649f9de
Yutaka Hara <yutaka.hara+github@gmail.com>
2024-05-10 18:23:57 +0900

    wip

--- chiika_runtime/src/sync_functions.rs
+++ chiika_runtime/src/sync_functions.rs
@@ -1,4 +1,23 @@
+use rand::{Rng, SeedableRng};
+use rand_chacha;
+
 #[no_mangle]
 pub extern "C" fn print(n: i64) {
     println!("{}", n);
+}
+
+#[no_mangle]
+pub extern "C" fn rand_int(n: i64) -> i64 {
+    match std::env::var("CHIIKA_RAND_SEED") {
+        Ok(seed) => {
+            let seed: u64 = seed.parse().unwrap();
+            println!("[chiika_runtime] Using seed: {}", seed);
+            let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(seed);
+            rng.gen_range(0..n)
+        }
+        _ => {
+            let mut rng = rand::thread_rng();
+            rng.gen_range(0..n)
+        }
+    }
 }
